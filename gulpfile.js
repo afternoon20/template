d4d7fe51390f0465eb49ef1fe8b6e322b3edbb7c
Takocheese <51657737+Takocheese@users.noreply.github.com>
2020-06-22 09:43:42 +0900

    シングルコーテーションに統一

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,29 @@
 // プラグインの読み込み
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const sassGlob = require("gulp-sass-glob");
-const plumber = require("gulp-plumber");
-const notify = require("gulp-notify");
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
-const cssdeclsort = require("css-declaration-sorter");
+const cssdeclsort = require('css-declaration-sorter');
 const browserSync = require('browser-sync');
 
-gulp.task("sass", function () {
-  return gulp.src("./sass/**/*.scss")
-    .pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
     .pipe(sassGlob())
     .pipe(sass({ outputStyle: 'expanded' }))
     .pipe(postcss([autoprefixer()]))
-    .pipe(postcss([cssdeclsort({ order: "alphabetical" })]))
-    .pipe(gulp.dest("./css"));
+    .pipe(postcss([cssdeclsort({ order: 'alphabetical' })]))
+    .pipe(gulp.dest('./css'));
 });
 
 gulp.task('browser-sync', function (done) {
   browserSync.init({
     server: {
-      baseDir: "./",
-      index: "index.html"
+      baseDir: './',
+      index: 'index.html'
     }
   });
   done();
@@ -35,13 +35,13 @@ gulp.task('bs-reload', function (done) {
 });
 
 
-gulp.task("watch", function () {
-  gulp.watch("./sass/**/*.scss", gulp.task("sass"));
-  gulp.watch("./sass/**/*.scss", gulp.task("bs-reload"));
-  gulp.watch("./*.html", gulp.task("bs-reload"));
-  gulp.watch("./css/*.css", gulp.task('bs-reload'));
-  gulp.watch("./js/*.js", gulp.task("bs-reload"));
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.scss', gulp.task('sass'));
+  gulp.watch('./sass/**/*.scss', gulp.task('bs-reload'));
+  gulp.watch('./*.html', gulp.task('bs-reload'));
+  gulp.watch('./css/*.css', gulp.task('bs-reload'));
+  gulp.watch('./js/*.js', gulp.task('bs-reload'));
 
 });
 
-gulp.task("default", gulp.series(gulp.parallel("browser-sync", "watch")));
+gulp.task('default', gulp.series(gulp.parallel('browser-sync', 'watch')));
